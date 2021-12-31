5dd670abe398cd09d521fdc69ceced5ac8237322
Ramesh Borukati <96637945+rameshbigdata@users.noreply.github.com>
2021-12-31 07:16:42 +0530

    Add files via upload

---
+++ src/com/ramesh/EmployeeCTC/EmpInfoDriver.java
@@ -0,0 +1,43 @@
+package com.ramesh.ctc;
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class EmpInfoDriver
+{
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException
+	{
+		Configuration conf = new Configuration();
+//		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+//		if (otherArgs.length != 2)
+//		{
+//			System.err.println("Usage: Employee Salary Anaysis <input> <output>");
+//			System.exit(2);
+//		}
+		Job job = new Job(conf, "Employee Salary Anaysis");
+		job.setJobName("Custmom Patitioner");
+		job.setJarByClass(EmpInfoDriver.class);
+		job.setMapperClass(EmpInfoMapper.class);
+		job.setReducerClass(EmpInfoReducer.class);
+		job.setPartitionerClass(EmpInfoPartitioner.class);				//Set custom partitioner class
+
+		job.setNumReduceTasks(2);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(Text.class);
+		
+		FileInputFormat.addInputPath(job, new Path("/home/hadoop/work/MRPrograms/EmployeeCTCAnalysis/input"));
+		FileOutputFormat.setOutputPath(job, new Path("/home/hadoop/work/MRPrograms/EmployeeCTCAnalysis/output"));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
